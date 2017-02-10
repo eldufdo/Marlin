d009733619ead5447c474cde071e09d1d68362ec
eldufdo <david.thaller@servus.at>
2017-02-10 19:18:32 +0100

    adjust schultis config

--- Marlin/example_configurations/schulti/Configuration.h
+++ Marlin/example_configurations/schulti/Configuration.h
@@ -388,6 +388,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = false; // set to true to invert the logic o
   #define X_PROBE_OFFSET_FROM_EXTRUDER 0.0
   #define Y_PROBE_OFFSET_FROM_EXTRUDER 0.0
   #define Z_PROBE_OFFSET_FROM_EXTRUDER 0.00  // Increase this if the first layer is too thin.
+  #define Z_PROBE_OFFSET_FROM_EXTRUDER_DEFAULT 0.0
 
   #define Z_RAISE_BEFORE_HOMING 4       // (in mm) Raise Z before homing (G28) for Probe Clearance.
                                         // Be sure you have this distance over your Z_MAX_POS in case
